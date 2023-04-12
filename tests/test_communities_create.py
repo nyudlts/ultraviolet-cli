c0f2523f913c60e49ee1b9f12c83d8b8142dbfcd
AkshayUHegde <akshayudayhegde2@gmail.com>
2023-04-12 15:44:18 -0400

    Corrected import

--- tests/test_communities_create.py
+++ tests/test_communities_create.py
@@ -1,9 +1,9 @@
-from ultraviolet_cli.commands.communities_create import communities_create
+from ultraviolet_cli.commands.communities_create import create_communities
 
 
 def test_cli_create_communities(app):
     """Test create user CLI."""
     runner = app.test_cli_runner()
 
-    result = runner.invoke(communities_create.create_communities, ["testcommunity", "--desc", "Test Community"])
+    result = runner.invoke(create_communities, ["testcommunity", "--desc", "Test Community"])
     assert result.exit_code != 0
